747106516503f1e0401765a2b21266f4fc2dc28d
Xavier Truong <truong.xavier@gmail.com>
2024-08-19 19:09:58 +0200

    ajout graphe et ocr

--- notebooks/dataset.py
+++ notebooks/dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import os
+import pytesseract
 
 
 chemin_images = '/mnt/d/DVPT/DST/images/'
@@ -25,4 +26,30 @@ plt.figure(figsize = (8,5))
 
 plt.imshow(df['image'].iloc[1], cmap='gray')
 
+df.head()
+df['label'].value_counts().head(15).plot(kind='bar')
+plt.xlabel('Label')
+plt.ylabel('Count')
+plt.title('Distribution of Labels')
+plt.show()
+
+df['shape'].value_counts().plot(kind='bar')
+plt.xlabel('Shape')
+plt.ylabel('Count')
+plt.title('Distribution of Shapes')
+plt.show()
+
+df['image'].apply(lambda x: plt.hist(x.flatten(), bins=256, color='gray', alpha=0.7))
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+plt.title('Pixel Value Distribution')
+plt.show()
+
+def ocr_image(image):
+    if image is not None:
+        return pytesseract.image_to_string(image)
+    else:
+        return None
+
+df['ocr_text'] = df['image'].apply(ocr_image)
 df.head()
